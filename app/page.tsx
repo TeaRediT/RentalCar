7ba9226d69527946bb825770c20ea7684d8193d0
TeaRediT <vlad.bobro.02@gmail.com>
2026-05-11 21:27:25 +0300

    add main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <></>;
-}
+import Image from "next/image";
+import css from "./home.module.css";
+
+const Home = () => {
+  return (
+    <main>
+      <div className={`container ${css.hero}`}>
+        <Image
+          className={css.img}
+          src="/home/hero.webp"
+          alt="car"
+          priority
+          fill
+        />
+        <h1 className={css.title}>Find your perfect rental car</h1>
+        <p className={css.slogan}>
+          Reliable and budget-friendly rentals for any journey
+        </p>
+        <button>View Catalog</button>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
