f903f9baf630cdf6bf9c9232799f969cffc06345
TeaRediT <vlad.bobro.02@gmail.com>
2026-05-16 17:23:00 +0300

    add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+import css from "./not-found.module.css";
+import ButtonLink from "@/components/ButtonLink/ButtonLink";
+
+export default function NotFound() {
+  return (
+    <main className={css.container}>
+      <div className={`container ${css.page}`}>
+        <h1 className={css.title} aria-label="Error 404">
+          404
+        </h1>
+        <h2 className={css.subtitle}>Page Not Found</h2>
+        <p className={css.description}>
+          Sorry, the page you are looking for does not exist, has been removed,
+          or is temporarily unavailable.
+        </p>
+        <ButtonLink href="/" className={css.btn}>
+          Return to Home
+        </ButtonLink>
+      </div>
+    </main>
+  );
+}
